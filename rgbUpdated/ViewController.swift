187f911199d31e49987c772fcaa4c67a65c94896
Mark <papushman@yandex.ru>
2023-03-29 23:15:20 +0300

    make UI elements work

--- rgbUpdated/ViewController.swift
+++ rgbUpdated/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var changingColorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changingColorView.layer.cornerRadius = 20
+        
+        changingColorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
     }
+    
 
+    
+    @IBAction func sliderAction (_ sender: UISlider) {
+        switch sender {
+        case redSlider:
+            setViewColor()
+            redValueLabel.text = String(format: "%.2f", redSlider.value)
+        case greenSlider:
+            setViewColor()
+            greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        default:
+            setViewColor()
+            blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        }
+    }
+    
+    private func setViewColor() {
+        changingColorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 
 }
 
